473ecf72f5b1c17c6b2d54b2311e720b9b09b65d
Sergey Lapin <lapin@tochka.com>
2019-05-18 15:47:12 +0500

    add visibility binding

--- android/app/src/main/java/com/rzhd/poi/presentation/auth/AuthViewModel.kt
+++ android/app/src/main/java/com/rzhd/poi/presentation/auth/AuthViewModel.kt
@@ -1,11 +1,14 @@
 package com.rzhd.poi.presentation.auth
 
 import com.rzhd.poi.core.event.ViewEventStartAuth
+import com.rzhd.poi.core.lifecycle.notNullLiveData
 import com.rzhd.poi.core.vm.BaseViewModel
 import com.rzhd.poi.presentation.base.MainScreen
 import ru.terrakok.cicerone.Router
 
 class AuthViewModel(private val router: Router) : BaseViewModel() {
+
+    val needShowLoading by notNullLiveData(false)
 
     fun onLoginClick() = postViewEvents(ViewEventStartAuth)
 
